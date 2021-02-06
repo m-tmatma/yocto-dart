acc1a51044b12cd94303fc37dfba8022f4deb657
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-02-06 20:05:20 +0900

    use SOURCE_MIRROR_URL.

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -9,19 +9,20 @@ cd $SCRIPT_DIR/var-fsl-yocto
 MACHINE=imx6ul-var-dart DISTRO=fsl-imx-fb . var-setup-release.sh -b build_fb
 #MACHINE=imx6ul-var-dart DISTRO=fsl-framebuffer . var-setup-release.sh build_fb
 
-TARGET_SSTATE_DIR=/home/shared/sstate-cache
-TARGET_DL_DIR=/home/shared/downloads
-
+# TARGET_SSTATE_DIR=/home/shared/sstate-cache
+# TARGET_DL_DIR=/home/shared/downloads
 rm -f conf/site.conf
-if [ -e "$TARGET_SSTATE_DIR" ] ; then
-    echo SSTATE_DIR=\"$TARGET_SSTATE_DIR\" >> conf/site.conf
-fi
-if [ -e "$TARGET_DL_DIR" ] ; then
-    echo DL_DIR=\"$TARGET_DL_DIR\" >> conf/site.conf
-fi
+# if [ -e "$TARGET_SSTATE_DIR" ] ; then
+#     echo SSTATE_DIR=\"$TARGET_SSTATE_DIR\" >> conf/site.conf
+# fi
+# if [ -e "$TARGET_DL_DIR" ] ; then
+#     echo DL_DIR=\"$TARGET_DL_DIR\" >> conf/site.conf
+# fi
+echo 'SOURCE_MIRROR_URL = "file:///home/shared/SOURCE_MIRROR_URL"' >> conf/site.conf
+echo 'INHERIT += "own-mirrors"' >> conf/site.conf
 
-bitbake meta-toolchain
+bitbake meta-toolchain --runall=fetch
 #./tmp/deploy/sdk/fsl-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
-./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
+#./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
 
-bitbake core-image-minimal
+bitbake core-image-minimal --runall=fetch
--- yocto-shell.sh
+++ yocto-shell.sh
@@ -13,15 +13,18 @@ CONTAINER_NAME=$(basename $REMOTE_URL)--${BRANCH_NAME}--$(basename $SCRIPT_DIR)_
 HOST_DOCKER_OPT=$SCRIPT_DIR/container/opt
 TARGET_OPT=/opt
 
-HOST_SSTATE_DIR=$HOME/shared/$IMAGEBASE/sstate-cache
-HOST_DL_DIR=$HOME/shared/$IMAGEBASE/downloads
-TARGET_SSTATE_DIR=/home/shared/sstate-cache
-TARGET_DL_DIR=/home/shared/downloads
+# HOST_SSTATE_DIR=$HOME/shared/$IMAGEBASE/sstate-cache
+# HOST_DL_DIR=$HOME/shared/$IMAGEBASE/downloads
+# TARGET_SSTATE_DIR=/home/shared/sstate-cache
+# TARGET_DL_DIR=/home/shared/downloads
+HOST_SOURCE_MIRROR_URL=$HOME/shared/$IMAGEBASE/SOURCE_MIRROR_URL
+TARGET_SOURCE_MIRROR_URL=/home/shared/SOURCE_MIRROR_URL
 
 mkdir -p $HOST_DOCKER_HOME
 mkdir -p $HOST_DOCKER_OPT
-mkdir -p $HOST_SSTATE_DIR
-mkdir -p $HOST_DL_DIR
+# mkdir -p $HOST_SSTATE_DIR
+# mkdir -p $HOST_DL_DIR
+mkdir -p $HOST_SOURCE_MIRROR_URL
 
 COMMAND_ARG=$1
 if [ x$COMMAND_ARG = x"build" ] ; then
@@ -50,6 +53,6 @@ docker run $ADDITIONAL_OPT --rm -u yocto:yocto \
 	--name $CONTAINER_NAME \
 	-v $HOST_SSTATE_DIR:$TARGET_SSTATE_DIR \
 	-v $HOST_DOCKER_OPT:$TARGET_OPT \
-	-v $HOST_DL_DIR:$TARGET_DL_DIR \
+	-v $HOST_SOURCE_MIRROR_URL:$TARGET_SOURCE_MIRROR_URL \
 	-v $HOST_DOCKER_HOME:$TARGET_HOME \
 	$X11OPTIONS -w $TARGET_HOME $DOCKERIMAGE $COMMAND_LINE
